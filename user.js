3486f5b20928c0d8c4933ddb8661b9ee1d6321d1
shaza <shazalugman@gmail.com>
2024-11-09 21:04:43 +0300

    Update user.js
    added connectToDatabase function, findUserByVerificationToken function, updateUserPassword function

--- user.js
+++ user.js
@@ -1,29 +1,58 @@
 const { MongoClient, ObjectId } = require("mongodb");
-const client = new MongoClient(process.env.MONGO_URI); //not sure about this line i googled it.
-const db = client.db("language_exchange");
-const usersCollection = db.collection("users");
 
-async function createUser(user){
-  await client.connect();
-  return usersCollection.insertOne(user);
-};
-async function findUserByEmail(email){
-  await client.connect();
-  return usersCollection.findOne({ email });
-};
-async function updateUserVerification(userId){
-  await client.connect();
-  return usersCollection.updateOne({ _id: new ObjectId(userId) }, { $set: { isVerified: true } });
-};
+let client;
+let db;
+let usersCollection;
+let sessionsCollection;
+
+async function connectToDatabase() {
+    if (!client) {
+        client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        db = client.db("language_exchange");
+        usersCollection = db.collection("users");
+        sessionsCollection = db.collection("sessions");
+    }
+}
+
+async function createUser(user) {
+    await connectToDatabase();
+    return usersCollection.insertOne(user);
+}
+
+async function findUserByEmail(email) {
+    await connectToDatabase();
+    return usersCollection.findOne({ email });
+}
+
+async function updateUserVerification(userId) {
+    await connectToDatabase();
+    return usersCollection.updateOne({ _id: new ObjectId(userId) }, { $set: { isVerified: true } });
+}
 
 async function createSession(userId) {
-  session = await db.collection('sessions').insertOne({
-    userId: userId,
-    createdAt: new Date()
-  });
-  return session;
+    await connectToDatabase();
+    const session = await sessionsCollection.insertOne({
+        userId: new ObjectId(userId),
+        createdAt: new Date()
+    });
+    return session;
+}
+
+async function findUserByVerificationToken(verificationToken) {
+    await connectToDatabase();
+    return usersCollection.findOne({ verificationToken });
+}
+
+async function updateUserPassword(userId, newPassword) {
+    await connectToDatabase();
+    return usersCollection.updateOne({ _id: new ObjectId(userId) }, { $set: { password: newPassword } });
 }
 
 module.exports= { createUser, 
                  findUserByEmail, 
-                 updateUserVerification};
+                 updateUserVerification,
+                 createSession,
+                 findUserByVerificationToken,
+                 updateUserPassword
+                };
