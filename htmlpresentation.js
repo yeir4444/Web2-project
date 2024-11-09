da47541961111d0f6547307457554659e91fef7f
SaraAlameen7 <saramohamed0802@gmail.com>
2024-11-09 18:26:16 +0300

    Added the login app.get() and app.post() functions to the htmlpresentation.js page

--- htmlpresentation.js
+++ htmlpresentation.js
@@ -5,3 +5,33 @@ const app = express();
 
 app.set('views', __dirname + "/templates");
 app.set('view engine', 'handlebars');
+
+app.get('/login', (req, res) => {
+    // Rendering the login page
+    res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    
+    try {
+        let sessionId = await business.login(username, password);
+        if (sessionId) {
+            res.cookie('sessionId', sessionId, {httpOnly: true});
+            res.send('Login successful');
+            res.redirect('/account');
+        }
+        else {
+            res.status(400).send('Invalid credentials')
+            res.redirect('/login');
+        }
+    }
+    catch (error) {
+        res.status(500).send('Login process error');
+        res.redirect('/login');
+    }
+});
+
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
