8870784e5fc6c8c90038ebbf8d740cedac65c42a
SaraAlameen7 <saramohamed0802@gmail.com>
2024-11-10 12:52:16 +0300

    Update htmlpresentation.js

--- htmlpresentation.js
+++ htmlpresentation.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
+     
 // Set up view engine
 app.set('views', __dirname + "/templates");
 app.set('view engine', 'handlebars');
