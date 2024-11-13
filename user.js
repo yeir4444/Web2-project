00772a66f206bb476ad90a5e4b85eda9a2586ae7
shaza <shazalugman@gmail.com>
2024-11-13 16:23:05 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,4 +1,5 @@
 const { MongoClient, ObjectId } = require("mongodb");
+const bcrypt = require('bcrypt');
 
 let client;
 let db;
@@ -7,52 +8,99 @@ let sessionsCollection;
 
 async function connectToDatabase() {
     if (!client) {
-        client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        client = new MongoClient('mongodb+srv://60301815:60301815@60301815.ndtwl.mongodb.net/');
         await client.connect();
-        db = client.db("language_exchange");
-        usersCollection = db.collection("users");
-        sessionsCollection = db.collection("sessions");
+        db = client.db('language_exchange');
+        usersCollection = db.collection('users');
+        sessionsCollection = db.collection('sessions');
     }
 }
 
 async function createUser(user) {
     await connectToDatabase();
-    return usersCollection.insertOne(user);
+    try {
+        // Hash the password before saving the user
+        user.password = await bcrypt.hash(user.password, 10);
+        return await usersCollection.insertOne(user);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        throw error;
+    }
 }
 
 async function findUserByEmail(email) {
     await connectToDatabase();
-    return usersCollection.findOne({ email });
+    try {
+        return await usersCollection.findOne({ email });
+    } catch (error) {
+        console.error("Error finding user by email:", error);
+        throw error;
+    }
 }
 
 async function updateUserVerification(userId) {
     await connectToDatabase();
-    return usersCollection.updateOne({ _id: new ObjectId(userId) }, { $set: { isVerified: true } });
+    try {
+        return await usersCollection.updateOne({ _id: ObjectId(userId) }, { $set: { isVerified: true } });
+    } catch (error) {
+        console.error("Error updating user verification:", error);
+        throw error;
+    }
 }
 
 async function createSession(userId) {
     await connectToDatabase();
-    const session = await sessionsCollection.insertOne({
-        userId: new ObjectId(userId),
-        createdAt: new Date()
-    });
-    return session;
+    try {
+        const session = await sessionsCollection.insertOne({
+            userId: ObjectId(userId),
+            createdAt: new Date()
+        });
+        return session;
+    } catch (error) {
+        console.error("Error creating session:", error);
+        throw error;
+    }
 }
 
 async function findUserByVerificationToken(verificationToken) {
     await connectToDatabase();
-    return usersCollection.findOne({ verificationToken });
+    try {
+        return await usersCollection.findOne({ verificationToken });
+    } catch (error) {
+        console.error("Error finding user by verification token:", error);
+        throw error;
+    }
 }
+
 
 async function updateUserPassword(userId, newPassword) {
     await connectToDatabase();
-    return usersCollection.updateOne({ _id: new ObjectId(userId) }, { $set: { password: newPassword } });
+    try {
+        // Hash the new password before updating
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        return await usersCollection.updateOne({ _id: ObjectId(userId) }, { $set: { password: hashedPassword } });
+    } catch (error) {
+        console.error("Error updating user password:", error);
+        throw error;
+    }
+}
+async function findSessionById(sessionId) {
+    await connectToDatabase();
+    return sessionsCollection.findOne({ _id: ObjectId(sessionId) });
 }
 
-module.exports= { createUser, 
-                 findUserByEmail, 
-                 updateUserVerification,
-                 createSession,
-                 findUserByVerificationToken,
-                 updateUserPassword
-                };
+async function findUserById(userId) {
+    await connectToDatabase();
+    return usersCollection.findOne({ _id: ObjectId(userId) });
+}
+
+module.exports = {
+    createUser,
+    findUserByEmail,
+    updateUserVerification,
+    createSession,
+    findUserByVerificationToken,
+    updateUserPassword,
+    findSessionById,
+    findUserById
+};
